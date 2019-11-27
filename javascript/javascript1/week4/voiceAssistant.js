fa9c7260df8a42908fbab3aa6e73ee355647f7ab
SwathiSankararaman <swathisankarraman@gmail.com>
2019-11-27 14:49:19 +0100

    formatting fixes

--- javascript/javascript1/week4/voiceAssistant.js
+++ javascript/javascript1/week4/voiceAssistant.js
@@ -5,7 +5,7 @@ let events = [];
 
 function getReply(command) {
     let splitCommand = command.split(' ');
-    
+
 
     // Question 1 (Hello my name is Benjamin)
     if (splitCommand[0] == 'Hello') {
@@ -33,19 +33,13 @@ function getReply(command) {
                 } else {
                     return ('Name cannot be empty!');
                 }
-
             }
+        } else if (splitCommand.includes('todo') || splitCommand.includes('todo?')) { // Question 6 (What is on my todo?)
 
-        }
-
-        // Question 6 (What is on my todo?)
-        if (splitCommand.includes('todo') || splitCommand.includes('todo?')) {
             let count = todo.length;
             return (`You have ${count} todos - ${todo.toString()}`);
-        }
 
-        // Question 7 (What day is it today?)
-        if (splitCommand.includes('day') && ((splitCommand.includes('today') || splitCommand.includes('today?')))) {
+        } else if (splitCommand.includes('day') && ((splitCommand.includes('today') || splitCommand.includes('today?')))) {  // Question 7 (What day is it today?)
             let day = new Date();
             let year = day.getFullYear();
             let date = day.getDate();
@@ -64,15 +58,14 @@ function getReply(command) {
                 'December'];
             let monthAsString = monthsInYear[month];
             return (`${date}. of ${monthAsString} ${year}`);
-        }
 
-        // Question 8 (what is 3 + 3?) 
-        if (splitCommand.includes('+') || splitCommand.includes('-') || splitCommand.includes('*') || splitCommand.includes('/')) {
+        } else if (splitCommand.includes('+') || splitCommand.includes('-') || splitCommand.includes('*') || splitCommand.includes('/')) { // Question 8 (what is 3 + 3?) 
+
             let operator = splitCommand.splice(3, 1); // now splitCommand is ["What", "is", "3", "3?"]
             let x = splitCommand.splice(2, 1); // ["What", "is", "3?"]
             let y = splitCommand.splice(2, 1);
-            x = parseInt(x, 10); // Converts string to whole integer(In our case '3' to 3)
-            y = parseInt(y, 10);
+            x = parseFloat(x); // Converts string to whole integer(In our case '3' to 3)
+            y = parseFloat(y);
             if (operator == '+') {
                 let sum = x + y;
                 return sum;
@@ -86,17 +79,12 @@ function getReply(command) {
                 let div = x / y;
                 return div;
             }
-        }
+        } else if (splitCommand.includes('favorite') && (splitCommand.includes('dish') || splitCommand.includes('dish?'))) {  // Question 10 (What is my favorite dish)
 
-        // Question 10 (What is my favorite dish)
-        if (splitCommand.includes('favorite') && (splitCommand.includes('dish') || splitCommand.includes('dish?'))) {
             return favouriteDish;
 
-        }
+        } else if (splitCommand.includes('doing') && (splitCommand.includes('week') || splitCommand.includes('week?'))) { // Question 13 (What am I doing this week?)
 
-
-        // Question 13 (What am I doing this week?)
-        if (splitCommand.includes('doing') && (splitCommand.includes('week') || splitCommand.includes('week?'))) {
             let length = events.length;
             for (let i = 0; i < length; i++) {
                 return (`This week you have ${length} event: ${events[i].name} on ${events[i].date}`);
@@ -108,10 +96,10 @@ function getReply(command) {
         // Question 12 (Add Bike ride on 3/5/2019 to my calendar)
         if (splitCommand.includes('calendar')) {
             splitCommand.splice(0, 1); // Removing Add (Bike ride on 3/5/2019 to my calendar)
-            splitCommand.splice((splitCommand.length) -3, 3); // Removing to my calendar (Bike ride on 3/5/2019)
-            let eventDate = splitCommand.splice((splitCommand.length) -1, 1); // taking bike ride on from array 
+            splitCommand.splice((splitCommand.length) - 3, 3); // Removing to my calendar (Bike ride on 3/5/2019)
+            let eventDate = splitCommand.splice((splitCommand.length) - 1, 1); // taking bike ride on from array 
             let date = eventDate.toString(); // Converting date to string
-            splitCommand.splice((splitCommand.length) -1, 1);
+            splitCommand.splice((splitCommand.length) - 1, 1);
             let name = splitCommand.join(' '); // converting it to string with space between bike and ride
             events.push({ 'name': name, 'date': date });
             return (`${name} added to your calendar`);
@@ -146,6 +134,7 @@ function getReply(command) {
         }
         // Question 9 (My favorite dish is lasagne)
     } else if (splitCommand.includes('favorite') && splitCommand.includes('dish')) {
+
         let dishName = splitCommand.splice(4, 1);
         dishName = dishName.toString();
         favouriteDish = dishName;
@@ -153,18 +142,19 @@ function getReply(command) {
 
         // Question 11 (Set a timer for 4 minutes)
     } else if (splitCommand.includes('Set') && splitCommand.includes('timer')) {
+
         let time = splitCommand.splice(4, 1); // To take the minutes to set the times(In our case 4)
         time = parseInt(time, 10); // Converts 4 string into number 4 which we can use in our timer
         let timeInMs = time * 60 * 1000; //4 * 60 * 1000 = 240000ms 
-        let id = setTimeout(function () {console.log('Timer done')}, timeInMs);
+        let id = setTimeout(function () { console.log('Timer done') }, timeInMs);
         return (`Timer set for ${time} minutes`);
 
-
-      // Question 14 (Add one or more command to your voice assistant) (How are you feeling?) (Tried random emotions generator)
+        // Question 14 (Add one or more command to your voice assistant) (How are you feeling?) (Tried random emotions generator)
     } else if (splitCommand.includes('How') && (splitCommand.includes('feeling') || splitCommand.includes('feeling?'))) {
-        let emotions = ['Happy', 'Excited', 'Surprised', 'Sad', 'Angry', 'Scared', 'Guilty', 'Disgusted', 'Romantic' , 'Blessed'];
+
+        let emotions = ['Happy', 'Excited', 'Surprised', 'Sad', 'Angry', 'Scared', 'Guilty', 'Disgusted', 'Romantic', 'Blessed'];
         let diffEmotions = Math.floor(Math.random() * emotions.length);
-        return emotions[diffEmotions];
+        return `I am feeling ${emotions[diffEmotions]}`;
 
     }
 
@@ -177,7 +167,7 @@ console.log(getReply('Add singing in the shower to my todo'));
 console.log(getReply('Remove fishing from my todo'));
 console.log(getReply('What is on my todo?'));
 console.log(getReply('What day is it today?'));
-console.log(getReply('What is 4 * 12?'));
+console.log(getReply('What is 4.5 * 12?'));
 console.log(getReply('My favorite dish is lasagne'));
 console.log(getReply('What is my favorite dish'));
 console.log(getReply('Set a timer for 4 minutes'));
