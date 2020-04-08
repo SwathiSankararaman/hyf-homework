b9b2d2c6a5f295cde09df24d6adbaa480fc13ff4
Swathi Raghavan <swathiraghavan@Swathis-Air.domain_not_set.invalid>
2020-04-08 06:53:27 +0200

    Implemented the feedback

--- react/week2/my_first_app/src/index.js
+++ react/week2/my_first_app/src/index.js
@@ -9,23 +9,19 @@ const root = document.getElementById('root');
 const todosArray = [
   {
     "id": 1,
-    "description": "Get out of bed"
+    "description": "Get out of bed",
+    "completed": false
+
   },
   {
     "id": 2,
-    "description": "Brush teeth"
+    "description": "Brush teeth",
+    "completed": false
   },
   {
     "id": 3,
-    "description": "Eat breakfast"
-  }, 
-  {
-    "id": 4,
-    "description": "Understand State and props"
-  },
-  {
-    "id": 5,
-    "description": "Passing the state through props"
+    "description": "Eat breakfast",
+    "completed": false
   }
 ];
 class Header extends React.Component {
@@ -33,55 +29,18 @@ class Header extends React.Component {
     return <h1>Todolist</h1>
   }
 }
-//I cannot pass a props that is in a state in one component and
-//set that props again in state in different component
-class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isChecked: false,
-      checked: []
-    };
-  }
 
-  handleCheckboxChange = (event) => {
-    const temp = event.target.checked
-    //Eventhough the item.id is int when it is passed inside name attribute in input tag
-    // it changes to string thats why parseInt
-    const specificTodo = parseInt(event.target.name)
-    //When I check the box my temp becomes true and executes if part
-    if (temp) {
-      //When checked I push that particular id to my checked array and update the state
-      this.state.checked.push(specificTodo)
-      this.setState({
-        isChecked: temp,
-        checked: this.state.checked
-      })
-    } else {
-      //When I uncheck, I get the index of the unchecked todo and remove it from checked array and update the state
-      const index = this.state.checked.indexOf(specificTodo)
-      this.state.checked.splice(index, 1);
-      this.setState({
-        isChecked: temp,
-        checked: this.state.checked
-      })
-    }
-  }
-
-  render() {
-    const array = this.props.todos;
-    const result = array.map((item) => {
-      return (
-        <div className='todo-list-display' key={item.id}>
-          <li style={{ 'textDecoration': this.state.checked.includes(item.id) ? 'line-through' : 'none' }}>{item.description}</li>
-          <label>
-            <input type="checkbox" name={item.id} onChange={this.handleCheckboxChange} defaultChecked={this.state.isChecked}></input>
-          </label>
-          <button onClick={() => this.props.onDeleteTodo(item.id)}>Delete</button>
-        </div>)
-    })
-    return <ul>{result}</ul>;
-  }
+function TodoListItem(props) {
+  const isComplete = props.todo.completed;
+  return (
+    <div>
+      <li style={{ 'textDecoration': isComplete ? 'line-through' : 'none' }}>{props.todo.description}</li>
+      <label>
+        <input type="checkbox" name={props.todo.id} onChange={props.onToggle}></input>
+      </label>
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  )
 }
 
 class Counter extends React.Component {
@@ -111,7 +70,7 @@ class Counter extends React.Component {
 }
 
 
-class Container extends React.Component {
+class TodoList extends React.Component {
   state = {
     todos: []
   };
@@ -121,38 +80,75 @@ class Container extends React.Component {
     //This if else is to check if I have rendered all todos in UI and if so
     //display alert 
     {
-      tempList.length > (todosArray.length) - 1 ? alert('No more Items in the Todo list!!') : 
-      this.setState({
-        todos: tempList.concat(todosArray[tempList.length])
-      })
+      tempList.length > (todosArray.length) - 1 ? alert('No more Items in the Todo list!!') :
+        this.setState({
+          todos: tempList.concat(todosArray[tempList.length])
+        })
     }
   }
 
   //This item comes as argument from the delete button onclick where i pass the item.id
-  handleDelete = (item) => {
+  handleDelete = (currentIndex) => {
     const newArray = this.state.todos.filter
-    (oneTodo => oneTodo.id !== item)
+      ((oneTodo, index) => currentIndex !== index)
     // newArray will have the items that didnot get deleted
     //So matching id is deleted and the rest is rerendered
-    this.setState ({
+    this.setState({
       todos: newArray
     })
+  }
 
+  handleToggle = (currentIndex) => {
+    const newArray = this.state.todos.map
+    ((oneTodo, index) => {
+      if (currentIndex === index) {
+        return {
+          ...oneTodo,
+          completed: !oneTodo.completed
+        }
+      } else {
+        return oneTodo
+      }
+    })
+    this.setState({
+      todos: newArray
+    })
   }
 
   render() {
     return (
-      <div className='todolist-container'>
-        <Header />
-        <Counter initialValue={0} />
+      <div>
         <button onClick={this.handleAdd}>Add todo</button>
-        {this.state.todos.length === 0 ? <p>No items</p> : <TodoList todos={this.state.todos} onDeleteTodo={this.handleDelete} />}
+        {this.state.todos.length === 0 ? <p>No items</p> : 
+        <div>
+          <ul>
+            {this.state.todos.map(
+            (todo, index) => (
+              <TodoListItem 
+              todo={todo} 
+              key={index}
+              onDelete={() => this.handleDelete(index)}
+              onToggle={() => this.handleToggle(index)}/>
+            ))}
+        </ul>
+        </div>
+        }
       </div>);
 
   }
 }
 
-ReactDOM.render(<Container />, root);
+function App() {
+  return (
+    <div>
+      <Header />
+      <Counter initialValue={0} />
+      <TodoList />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
