06a6d46e0e48c7bb0778050e7c649aa96bdea8db
Swathi Sankararaman <55897408+SwathiSankararaman@users.noreply.github.com>
2020-05-04 20:58:29 +0200

    Update movies.js

--- javascript/javascript3/week2/movies.js
+++ javascript/javascript3/week2/movies.js
@@ -2,7 +2,7 @@
 function fetchData() {
 
     fetch("https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json")
-        .then(Response => Response.json())
+        .then(response => response.json())
         .then(data => {
             console.log(data);
             organizeData(data);
