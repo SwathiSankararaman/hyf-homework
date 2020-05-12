4331f7ef3b2743ba635515c7c669a6ed1289f906
Swathi Sankararaman <55897408+SwathiSankararaman@users.noreply.github.com>
2020-05-12 15:33:29 +0200

    Included ul inside child component

--- react/week4/github_users_search/src/UserList.js
+++ react/week4/github_users_search/src/UserList.js
@@ -10,11 +10,13 @@ function UserList() {
         <div className='lists'>
             {isClickedUser ? <UserRepo /> :
                 <div>
+        <ul>
                     {usersArray.map(user =>
                         <li key={user.id} className='github_userlist'>
                             <a href={user.login} onClick={(event) => handleClickSearch(event, user.login)}>{user.login}
                                 <img alt="avatar" style={{ width: '50px' }} src={user.avatar_url} /></a>
                         </li>)}
+       </ul>
                 </div>}
         </div>
     )
